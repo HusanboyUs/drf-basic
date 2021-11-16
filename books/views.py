ceb76b691d55deafda7890b7429eb5b5347afb21
Usmonov Husanboy <70463121+HusanboyUs@users.noreply.github.com>
2021-11-16 13:46:27 +0100

    basic drf

---
+++ books/views.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book
+
+
+
+
+class BookListView(ListView):
+    model=Book
+    template_name='books/book_list.html'
+    
